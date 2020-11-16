482e2f12d74ed72a50661f63c358ed290ef9f36f
Celine Gianduzzo <cgianduzzo@gmail.com>
2020-11-16 14:55:44 +0100

    implemented test_me()

--- new_project/lib.py
+++ new_project/lib.py
@@ -47,6 +47,9 @@ def clean_data(data):
     data.loc[:, 'Frequency'] = data['Frequency'].map(drows)
     return data
 
+def try_me(name):
+    return "Hi f'{name}', you just tried my function!"
+
 
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
